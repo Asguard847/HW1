54d5ae626183c2e28889ff6d83bc4531d290b19b
Asguard847 <polniyplatsap@gmail.com>
2019-09-19 01:26:20 +0300

    Solved review issues

--- src/main/java/task5/ChangeBit.java
+++ src/main/java/task5/ChangeBit.java
@@ -1,35 +1,38 @@
 package task5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ChangeBit {
     public static void main(String[] args) {
         System.out.println("Enter number to be changed, than position of a bit");
-        try(Scanner sc = new Scanner(System.in)){
+        try (Scanner sc = new Scanner(System.in)) {
             int number = sc.nextInt();
             int pos = sc.nextInt();
 
-            //Here we find out if byte to be changed is set or not
-            int requiredByte = number >> pos & 1;
+            boolean byteIsSet = checkIfByteIsSet(number, pos);
 
             int result;
-            if(requiredByte==1){
+            if (byteIsSet) {
                 result = number & ~(1 << pos);
-            }else{
+            } else {
                 result = number | (1 << pos);
             }
             System.out.println("Resulting value is: " + result);
 
-        }catch (Exception e){
+        } catch (InputMismatchException e) {
             System.out.println("Wrong input");
         }
     }
 
-        //Another convenient method if we just need to switch a bit
-        public static int changeBit(int number, int pos){
-            return number^(1<<pos);
-
+    public static boolean checkIfByteIsSet(int number, int pos) {
+        int requiredByte = number >> pos & 1;
+        return requiredByte == 1 ? true : false;
     }
 
+    //Another convenient method if we just need to switch a bit
+    public static int changeBit(int number, int pos) {
+        return number ^ (1 << pos);
+    }
 
 }
